31716bf7244b14aaedd4c4ad1b7d8c758a889aa1
Märt Rang <rang501@hotmail.com>
2015-03-06 15:56:55 +0000

    bash argument

--- sql-import.sh
+++ sql-import.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 PROJECT=$1
+SSH_USER=$2
 
 # Generate SQL dump
-ssh arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
+ssh $SSH_USER@arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
 cd /var/www/arendus/${PROJECT}
 drush sql-dump --result-file=~/${PROJECT}_dump.sql
 ENDSSH
 # Copy dump to local machine
-scp arendus.fenomen.ee:${PROJECT}_dump.sql ~/${PROJECT}_dump.sql
+scp $SSH_USERarendus.fenomen.ee:${PROJECT}_dump.sql ~/${PROJECT}_dump.sql
 # Delete dump from dev
-ssh arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
+ssh $SSH_USERarendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
 rm ~/${PROJECT}_dump.sql
 ENDSSH
