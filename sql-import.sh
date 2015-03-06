fb7690c37b12e7fd94a1b8470c7197e0317f385c
Märt Rang <rang501@hotmail.com>
2015-03-06 14:29:04 +0000

    Typos

--- sql-import.sh
+++ sql-import.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-PROJECT = "ecofleet"
-SSH_USER = "user"
+PROJECT="ecofleet"
+SSH_USER="user"
 
 # Generate SQL dump
-ssh $USER@arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
+ssh $SSH_USER@arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
 cd /var/www/arendus/${PROJECT}
 drush sql-dump --result-file=~/${PROJECT}_dump.sql
 ENDSSH
 # Copy dump to local machine
-scp mart@arendus.fenomen.ee:${PROJECT}_dump.sql ~/${PROJECT}_dump.sql
+scp $SSH_USER@arendus.fenomen.ee:${PROJECT}_dump.sql ~/${PROJECT}_dump.sql
 # Delete dump from dev
-ssh $USER@arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
+ssh $SSH_USER@arendus.fenomen.ee PROJECT=$PROJECT 'bash -s' <<ENDSSH
 rm ~/${PROJECT}_dump.sql
 ENDSSH
